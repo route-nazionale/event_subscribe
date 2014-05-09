1180e845e974ed1bb1de37408edfe37ae2f13833
Luca Ferroni <luca@befair.it>
2014-05-09 17:36:35 +0200

    Basic admin interface for the base application!

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from base.models import Chief, SubCamp, ScoutGroup
+
+class ChiefAdmin(admin.ModelAdmin):
+
+    list_display = ('__unicode__', 'scout_group', 'code_membership')
+    list_filter = ('scout_group',)
+
+class GenericNameAdmin(admin.ModelAdmin):
+
+    list_display = ('__unicode__', 'name', 'n_objs')
+
+admin.site.register(Chief, ChiefAdmin)
+admin.site.register(SubCamp, GenericNameAdmin)
+admin.site.register(ScoutGroup, GenericNameAdmin)
+
+
--- base/models.py
+++ base/models.py
@@ -4,41 +4,74 @@ class ScoutGroup(models.Model):
 
     name = models.CharField(max_length=128, unique=True)
 
-    def __unicode__(self):
-        return self.name
-
     class Meta:
 
         verbose_name = "gruppo scout"
         verbose_name_plural = "gruppi scout"
 
+    def __unicode__(self):
+        return self.name
+
+    def n_chiefs(self):
+        return self.chief_set.count()
+    n_chiefs.short_description = "num. capi"
+
+    n_objs = n_chiefs
+
 #--------------------------------------------------------------------------------
 
 class Chief(models.Model):
 
-    code_membership = models.CharField(max_length=128, unique=True)
+    code_membership = models.CharField(max_length=128, unique=True,
+        verbose_name="codice censimento"
+    )
     scout_group = models.ForeignKey(ScoutGroup)
-
-    def __unicode__(self):
-        return "<%s> - %s" % (self.scout_group, self.code_membership)
 
     class Meta:
 
         verbose_name = "capo"
         verbose_name_plural = "capi"
 
+    def __unicode__(self):
+        return "<%s> - %s" % (self.scout_group, self.code_membership)
+
 #--------------------------------------------------------------------------------
 
 class SubCamp(models.Model):
+    """
+    Subcamp entity
+    """
 
     name = models.CharField(max_length=128, unique=True)
     
+    class Meta:
+
+        verbose_name = "sottocampo"
+        verbose_name_plural = "sottocampi"
+
     def __unicode__(self):
         return self.name
+
+    def n_events(self):
+        return self.event_set.count()
+    n_events.short_description = "num. eventi"
+
+    n_objs = n_events
 
 #--------------------------------------------------------------------------------
 
 class Event(models.Model):
+    """
+    Events can be laboratories or round tables, each one
+    with their own specifications.
+
+    They could be holded in different datetimes,
+    by current requirements it seems that they will last
+    for half-a-day but this model is planned to be versatile.
+
+    The same thing happens for tot_seats which seems to be
+    fixed (30 for a laboratory, 500 for a round table)
+    """
 
     EVENT_KIND_CHOICES = (
         ('lab', 'laboratorio'),
@@ -59,10 +92,11 @@ class Event(models.Model):
     dt_start = models.DateTimeField()
     dt_stop = models.DateTimeField()
 
-    def __unicode__(self):
-        return self.name
-
     class Meta:
 
         verbose_name = "evento"
         verbose_name_plural = "eventi"
+
+    def __unicode__(self):
+        return self.name
+
