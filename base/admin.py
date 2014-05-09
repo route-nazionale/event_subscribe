5146a073d8ced65d326367edb00837033e563ff7
Luca Ferroni <luca@befair.it>
2014-05-09 17:47:26 +0200

    Aggiunto il modello Event nella admin interface
    Enjoy a little :)
    
    (si era detto commenti misti no?!?)

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from base.models import Chief, SubCamp, ScoutGroup
+from base.models import Chief, SubCamp, ScoutGroup, Event
 
 class ChiefAdmin(admin.ModelAdmin):
 
@@ -11,8 +11,18 @@ class GenericNameAdmin(admin.ModelAdmin):
 
     list_display = ('__unicode__', 'name', 'n_objs')
 
+class EventAdmin(admin.ModelAdmin):
+
+    list_display = (
+        '__unicode__', 'name', 'dt_start', 'dt_stop', 'subcamp', 'n_seats', 'tot_seats'
+    )
+    list_editable = (
+        'name', 'dt_start', 'dt_stop', 'subcamp', 'n_seats', 'tot_seats'
+    )
+
 admin.site.register(Chief, ChiefAdmin)
 admin.site.register(SubCamp, GenericNameAdmin)
 admin.site.register(ScoutGroup, GenericNameAdmin)
+admin.site.register(Event, EventAdmin)
 
 
