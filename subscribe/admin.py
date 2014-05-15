2718e177461e6ba561323c1fc57eaca0e38ad2ca
Luca Ferroni <luca@befair.it>
2014-05-15 02:46:50 +0200

    Stub for clean() method to validate fields
    to be filled TODOs by newbies

--- subscribe/admin.py
+++ subscribe/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from subscribe.models import ScoutChiefEventHappeningSubscription
+from subscribe.models import ScoutChiefSubscription
 
-class ScoutChiefEventHappeningSubscriptionAdmin(admin.ModelAdmin):
+class ScoutChiefSubscriptionAdmin(admin.ModelAdmin):
 
     list_display = ('__unicode__', 'scout_chief', 'event', 'subscribed_on')
     list_display = ('__unicode__', 'subscribed_on',)
     #list_editable = ('scout_chief', 'event')
 
-admin.site.register(ScoutChiefEventHappeningSubscription, ScoutChiefEventHappeningSubscriptionAdmin)
+admin.site.register(ScoutChiefSubscription, ScoutChiefSubscriptionAdmin)
