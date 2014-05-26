68aac5ecaba6d6963160efce4c11047d615ddfe9
tapion <tapion@tapion.it>
2014-05-26 16:06:51 +0200

    Aggiunto typeahead per le unità nel form

--- base/views.py
+++ base/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from base.models import Event
+from base.models import Event, Unit
 from django.core import serializers
 from django.http import HttpResponse
+
+import json
 
 def events(request):
     events_as_json = serializers.serialize('json', Event.objects.all())
     return HttpResponse(events_as_json, content_type="application/json")
+
+def units(request):
+    units = []
+    for unit in Unit.objects.all():
+        units.append(unit.name)
+
+    return HttpResponse(json.dumps(units))
