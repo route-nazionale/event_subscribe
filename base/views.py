a0285d33d9a3b888a308e27f6451c47664d2cb8b
Luca Ferroni <luca@befair.it>
2014-06-05 19:24:16 +0200

    Filtri per gli eventi restituiti e poca nomenclatura

--- base/views.py
+++ base/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.db import models
 
-from base.models import EventHappening, Unit
+from base.models import EventHappening, Unit, Event
 from base.views_support import HttpJSONResponse
 
 
 def events(request):
     events = []
-    for eh in EventHappening.objects.all():
+    eh_qs = EventHappening.objects.all()
+    eh_qs = eh_qs.filter(event__state_chief__in=(Event.STATE_ENABLED, Event.STATE_RESERVED))
+    eh_qs = eh_qs.filter(event__state_activation=Event.ACTIVATION_ACTIVE)
+    eh_qs = eh_qs.filter(event__state_subscription=Event.SUBSCRIPTION_OPEN)
+    for eh in eh_qs:
         obj = eh.as_dict()
         events.append(obj)
     return HttpJSONResponse(events)
