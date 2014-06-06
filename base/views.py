c278493efb9c86dd2f7db158ef8cfc943e0eb254
Luca Ferroni <luca@befair.it>
2014-06-06 16:05:34 +0200

    Aggiunto filtro per restringere gli eventi restituiti a quelli con posti liberi

--- base/views.py
+++ base/views.py
@@ -6,15 +6,26 @@ from base.views_support import HttpJSONResponse
 
 
 def events(request):
+    """
+    Return all subscriptable events.
+    """
+
     events = []
     eh_qs = EventHappening.objects.all()
+    # Restrict to those enabled or reserved for chiefs
     eh_qs = eh_qs.filter(event__state_chief__in=(Event.STATE_ENABLED, Event.STATE_RESERVED))
+    # Restrict to those actives
     eh_qs = eh_qs.filter(event__state_activation=Event.ACTIVATION_ACTIVE)
+    # Restrict to those open for subscription
     eh_qs = eh_qs.filter(event__state_subscription=Event.SUBSCRIPTION_OPEN)
     for eh in eh_qs:
-        obj = eh.as_dict()
-        events.append(obj)
+        # Restrict to those with available seats
+        # I use properties!
+        if eh.available_seats:
+            obj = eh.as_dict()
+            events.append(obj)
     return HttpJSONResponse(events)
+
 
 def units(request):
     units = []
