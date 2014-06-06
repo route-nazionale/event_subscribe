03dc7f750fd3f3116ed6155f0d6ca307d61d7fea
Luca Ferroni <luca@befair.it>
2014-06-06 12:43:22 +0200

    Ultime modifiche ai modelli: subscription.is_locked chief.is_spalla
    * is_spalla -> non si può loggare perché viene iscritto dalla "pattuglia eventi"
    * is_locked -> non si può cancellare da un evento cui è iscritto

--- subscribe/models.py
+++ subscribe/models.py
@@ -9,6 +9,7 @@ class ScoutChiefSubscription(models.Model):
 
     scout_chief = models.ForeignKey(ScoutChief)
     event_happening = models.ForeignKey(EventHappening)
+    is_locked = models.BooleanField(default=False, verbose_name=u"non si può cancellare")
 
     subscribed_on = models.DateTimeField(auto_now_add=True)
     unsubscribed_on = models.DateTimeField(null=True, blank=True)
@@ -22,7 +23,7 @@ class ScoutChiefSubscription(models.Model):
         verbose_name_plural = "iscrizioni eventi"
 
     def __unicode__(self):
-        return "%s - %s" % (self.scout_chief, self.event_happening)
+        return u"%s - %s" % (self.scout_chief, self.event_happening)
 
     def clean(self):
         """
@@ -37,15 +38,22 @@ class ScoutChiefSubscription(models.Model):
         n_subscriptions = subscriptions.count()
 
         if n_subscriptions >= self.MAX_SUBSCRIPTIONS:
-            raise ValidationError('Non è possibile iscriversi a più di 3 eventi')
+            raise ValidationError(u'Non è possibile iscriversi a più di 3 eventi')
 
         #check no eventi sovrapposti
         if subscriptions.filter(event_happening__timeslot=self.event_happening.timeslot).count():
-            raise ValidationError('Sei già iscritto ad un evento di questo turno')
+            raise ValidationError(u'Sei già iscritto ad un evento di questo turno')
 
         #TODO: check che ci siano posti liberi
         
 
     def save(self, *args, **kw):
         self.full_clean()
-        super(ScoutChiefSubscription, self).save(*args, **kw)
+        return super(ScoutChiefSubscription, self).save(*args, **kw)
+
+    def delete(self):
+        if self.is_locked:
+            raise ValidationError(u"%s è vincolato a %s: non è possibile eliminarlo" % (
+                self.scout_chief, self.event_happening
+            ))
+        return super(ScoutChiefSubscription, self).delete()
